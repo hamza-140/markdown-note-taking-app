13fcb1122b49878f50428fe5bcd324e448d18906
hamza <sahamzashah19@gmail.com>
2024-09-05 17:29:09 +0500

    update

--- index.js
+++ index.js
@@ -15,6 +15,23 @@ app.listen(3000, () => {
   console.log("Listening on port 3000!");
 });
 
+app.get("/all",function(req,res){
+  res.sendFile(path.join(__dirname, "./list.html"));
+})
+
+app.get('/files', (req, res) => {
+  const directoryPath = path.join(__dirname, 'uploads');
+
+  fs.readdir(directoryPath, (err, files) => {
+      if (err) {
+          return res.status(500).send('Unable to scan directory');
+      }
+
+      // Send the file names as a JSON response
+      res.json(files.map(file => ({ name: file, group: 'Uploads', role: 'File' })));
+  });
+});
+
 // Handle the file upload
 app.post("/result", upload.single("uploadedFile"), function (req, res) {
   console.log("Uploading File");
